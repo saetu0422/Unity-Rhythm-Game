892491ad1046fe312c2bbf65138d4a101a1fefbc
Junsoi <83137169+saetu0422@users.noreply.github.com>
2021-12-05 21:14:56 +0900

    에셋 추가 및 초기화면

---
+++ Unity Rhythm Game/Assets/Scripts/BackGroundBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundBehavior : MonoBehaviour
+{
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+    void Start()
+    {
+        gameBackgroundSpriteRenderer = gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f));
+        
+    }
+
+//점점 흐려지게
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while (color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
