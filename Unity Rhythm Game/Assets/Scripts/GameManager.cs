dde9e6fd159c08ca70f15309a98ad8a751682f8a
Junsoi <83137169+saetu0422@users.noreply.github.com>
2021-12-05 23:44:52 +0900

    구분선 작업 & 노트 오브젝트 생성 및 이동

---
+++ Unity Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float noteSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
